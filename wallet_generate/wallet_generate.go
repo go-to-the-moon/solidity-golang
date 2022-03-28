7d300dad4d5e44f45737a79a1269ba451af6d992
niwenfeng <niwenfeng@hashquark.io>
2022-03-28 23:16:28 +0800

    transaction

--- wallet_generate/wallet_generate.go
+++ wallet_generate/wallet_generate.go
@@ -11,6 +11,7 @@ import (
 )
 
 func main() {
+	// 生成新的私钥
 	privateKey, err := crypto.GenerateKey()
 	if err != nil {
 		log.Fatal(err)
@@ -19,6 +20,8 @@ func main() {
 	privateKeyBytes := crypto.FromECDSA(privateKey)
 	fmt.Println(hexutil.Encode(privateKeyBytes)[2:]) // 0xfad9c8855b740a0b7ed4c221dbad0f33a83a49cad6b3fe8d5817ac83d38b6a19
 
+
+	// 私钥转公钥
 	publicKey := privateKey.Public()
 	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
 	if !ok {
@@ -28,6 +31,7 @@ func main() {
 	publicKeyBytes := crypto.FromECDSAPub(publicKeyECDSA)
 	fmt.Println(hexutil.Encode(publicKeyBytes)[4:]) // 0x049a7df67f79246283fdc93af76d4f8cdd62c4886e8cd870944e817dd0b97934fdd7719d0810951e03418205868a5c1b40b192451367f28e0088dd75e15de40c05
 
+	// 公钥转地址
 	address := crypto.PubkeyToAddress(*publicKeyECDSA).Hex()
 	fmt.Println(address) // 0x96216849c49358B10257cb55b28eA603c874b05E
 
