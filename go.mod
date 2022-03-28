ba7237a43effa7068e8113aa389406d20ff8e1da
niwenfeng <niwenfeng@hashquark.io>
2022-03-28 20:38:08 +0800

    hello world

---
+++ go.mod
@@ -0,0 +1,5 @@
+module solidity-golang
+
+go 1.15
+
+require github.com/ethereum/go-ethereum v1.10.16
---
+++ src/Client.go
@@ -0,0 +1,17 @@
+package src
+
+import (
+	"fmt"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"log"
+)
+
+func main() {
+	client, err := ethclient.Dial("https://cloudflare-eth.com")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("we have a connection")
+	_ = client // we'll use this in the upcoming sections
+}
